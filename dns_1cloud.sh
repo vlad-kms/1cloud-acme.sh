ffa49ebe6554535562f9ca032c0581cc152d6e0a
Vladimir Alexeev <9141778236@mai.ru>
2024-05-02 19:10:43 +1000

    v2.0.001 version 2 begin

--- dns_1cloud.sh
+++ dns_1cloud.sh
@@ -21,19 +21,14 @@ dns_1cloud_add() {
     _err "Please create you key and try again."
     return 1
   fi
-
   #save the api key to the account conf file.
   _saveaccountconf_mutable OC_Key "$OC_Key"
-
   _debug "First detect the root zone"
   if ! _get_root "$fulldomain"; then
     _err "invalid domain"
     return 1
   fi
   _debug _domain_id "$_domain_id"
-  _debug _sub_domain "$_sub_domain"
-  _debug _domain "$_domain"
-
   _info "Adding record"
   #if _oc_rest POST "/$_domain_id/records/" "{\"type\": \"TXT\", \"ttl\": 60, \"name\": \"$fulldomain\", \"content\": \"$txtvalue\"}"; then
   if _oc_rest POST "recordtxt/" "{\"DomainId\": \"$_domain_id\", \"TTL\": 60, \"Name\": \"$fulldomain\", \"Text\": \"$txtvalue\"}"; then
@@ -44,7 +39,7 @@ dns_1cloud_add() {
       return 0
     fi
   fi
-  _err "Add txt record error."
+  _err "add txt record error"
   return 1
 }
 
@@ -61,48 +56,27 @@ dns_1cloud_rm() {
     _err "Please create you key and try again."
     return 1
   fi
-
   _debug "First detect the root zone"
   if ! _get_root "$fulldomain"; then
     _err "invalid domain"
     return 1
   fi
   _debug _domain_id "$_domain_id"
-  _debug _sub_domain "$_sub_domain"
-  _debug _domain "$_domain"
   _debug "Getting txt records"
   #####################################
   # инфа о корневом домене и его записях
   _oc_rest GET "/${_domain_id}"
   _debug3 response "$response"
   if ! _contains "$response" "$txtvalue"; then
-    _err "Txt record not found"
-    return 1
+    _err "TXT record not found"
+    return 0
   fi
-  # все ресурсные записи домена
-  _record_all=$( echo "$response"   | sed -nE "s/.*(\"LinkedRecords\" *: *\[)([^]]*)\].*/\2/p")
-  _debug2 "_record_all" "$_record_all"
-  if [ -z "$_record_all" ]; then
-    _err "can not find _record_all"
-    return 1
-  fi
-  # ресурсная запись TXT acme*
-  _record_seg=$(echo "$_record_all" | sed -nE "s/.*([^{]*\"ID[^}]*\"Text\" *: *\"..$txtvalue[^}]*).*/\1/p")
-  #_record_seg="$(echo "$response" | _egrep_o "[^{]*\"content\" *: *\"$txtvalue\"[^}]*}")"
-  _debug2 "_record_seg" "$_record_seg"
-  if [ -z "$_record_seg" ]; then
-    _err "can not find _record_seg"
-    return 1
-  fi
-  # id записи
-  _record_id=$(echo "$_record_seg"   | sed -nE "s/.*\"ID\" *: *([[:digit:]]*).*/\1/p")
-  #_record_id="$(echo "$_record_seg" | tr "," "\n" | tr "}" "\n" | tr -d " " | grep "\"id\"" | cut -d : -f 2)"
+  _record_id=$(echo "$response" | sed -nE "s/.*(\"LinkedRecords\" *: *\[)([^]]*)\].*/\2/p" | sed -nE "s/.*(\{[^{]*\"ID\" *: *([[:digit:]]*),[^}]*\"Text\" *: *\"[\]\"${txtvalue}[^}]*\}).*/\2/p")
   _debug2 "_record_id" "$_record_id"
   if [ -z "$_record_id" ]; then
     _err "can not find _record_id"
     return 1
   fi
-
   if ! _oc_rest DELETE "/$_domain_id/$_record_id"; then
     _err "Delete record error."
     return 1
@@ -113,63 +87,30 @@ dns_1cloud_rm() {
 ####################  Private functions below ##################################
 #_acme-challenge.www.domain.com
 #returns
-# _sub_domain=_acme-challenge.www
-# _domain=domain.com
-# _domain_id=sdjkglgdfewsdfg
+# _domain     - корневой домен
+# _domain_id  - id домена
 _get_root() {
-  # проверить установлен ли jq
-  jq --version > /dev/null 2>&1
-  if [[ $? -ne 0 ]]; then
-    _err "error Reguired installed jq"
-    is_jq=0
-    #return 1
-  else
-    is_jq=1
-  fi;
-  # HACK установка в 0 отменяет применение jq даже при его наличии
-  #is_jq=0
-  _debug "Installed jq is" $is_jq
-
   domain=$1
-  # список доменов от 1Cloud
+  # у 1cloud корневым доменом может быть только домен 2-го уровня
+  # получаем корневой и субдомен домены для переданной записи
+  count_word="$(echo "$domain" | tr '.' ' ' | wc -w)"
+  if [ "$count_word" -lt 3 ]; then
+    _err "Error domain NAME"
+    return 1
+  fi
+  _domain=$(echo "$domain" | cut -d. -f $(("$count_word"-1))-100)
+  _debug _domain "$_domain"
+  # список зарегистрированных доменов на 1Cloud
   if ! _oc_rest GET "/"; then
+    _err "get domains from hostinger"
     return 1
   fi
-  # они поддерживают только домены 2-го уровня, поэтому корневой - это name.dom
-  # получаем корневой для переданной записи
-  old_IFS=$IFS
-  IFS='.' read -ra arr_dom <<< "$domain"
-  IFS=$old_IFS
-  s="${#arr_dom[*]}"
-  li=$(( $s - 1 ))
-  _d="${arr_dom[(( $li - 1 ))]}.${arr_dom[$li]}"
-  # _sub_domain
-  _sd=''
-  for (( i = 0; i < $(( $s - 2 )); i++ )); do
-    _sd="${_sd}.${arr_dom[$i]}"
-  done
-  _sd=${_sd:1:${#_sd}}
-  # надо найти запись для домена среди полученных доменов от провайдера в $response и в ней ID
-  # response='[{"ID":34124,"Name":"mrovo.ru",..., {"ID":341244,"Name":"mrovo.ru",...}]'
-  # _domain_id
-  if [[ $is_jq -eq 1 ]]; then
-    c=$(echo "$response" | jq '.| length')
-    for (( i = 0; i < ${c}; i++ )); do
-      e=$((echo "$response" | jq ".[$i]") | jq '.Name' | sed -nE "s/([\"]*)([^\"]*).*/\2/p")
-      if [ "$e" = "$_d" ]; then
-        _domain_id=$((echo "$response" | jq ".[$i]") | jq '.ID')
-        break
-      fi
-    done
-  else
-    _domain_id=$(echo "$response" | sed -nE "s/.*(\"ID\":)([[:digit:]]*)(.\"Name\":\"$_d\").*/\2/p")
-  fi
-  if [[ -z $_domain_id ]]; then
-    _err "error NOT domain ID"
+  # надо проверить наличие запись для домена среди полученных доменов от провайдера в $response и в ней получить ID
+  _domain_id=$(echo "$response" | sed -nE "s/.*(\"ID\":)([[:digit:]]*)(.\"Name\":\"$_domain\").*/\2/p")
+  if [ -z "$_domain_id" ]; then
+    _err "domain ID: is not"
     return 1
   fi
-  _domain=$_d
-  _sub_domain=$_sd
   return 0
 }
 
